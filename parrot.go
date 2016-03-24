a7c1237600414707709e937e4029c23fb4d75f11
gi4nks <gnlc.lupo@gmail.com>
2016-03-24 16:44:00 +0100

    Modified output

--- parrot.go
+++ parrot.go
@@ -3,7 +3,18 @@ package quant
 import (
 	"fmt"
 	//"os"
-	"github.com/ttacon/chalk"
+	"time"
+)
+
+var (
+	green   = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
+	white   = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
+	yellow  = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
+	red     = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
+	blue    = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
+	magenta = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
+	cyan    = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
+	reset   = string([]byte{27, 91, 48, 109})
 )
 
 func NewParrot(n string) *Parrot {
@@ -19,32 +30,57 @@ type Parrot struct {
 	debugMode bool
 }
 
+func colorForStatus(method string) string {
+	switch {
+	case method == "debug":
+		return green
+	case method == "info":
+		return white
+	case method == "warn":
+		return yellow
+	default:
+		return red
+	}
+}
+
 func (t Parrot) trace(a Action0) {
 	a()
 }
 
 func (t Parrot) Warn(a ...interface{}) {
 	t.trace(func() {
-		fmt.Print(chalk.Yellow)
+		end := time.Now()
+		statusColor := colorForStatus("warn")
+
+		fmt.Printf("[%v] |%s %s %s| ",
+			end.Format("2006/01/02 - 15:04:05"),
+			statusColor, "WA", reset)
 		fmt.Println(a...)
-		fmt.Print(chalk.Reset)
 	})
 }
 
 func (t Parrot) Error(a ...interface{}) {
 	t.trace(func() {
-		fmt.Print(chalk.Red)
+		end := time.Now()
+		statusColor := colorForStatus("error")
+
+		fmt.Printf("[%v] |%s %s %s| ",
+			end.Format("2006/01/02 - 15:04:05"),
+			statusColor, "ER", reset)
 		fmt.Println(a...)
-		fmt.Print(chalk.Reset)
 
 	})
 }
 
 func (t Parrot) Info(a ...interface{}) {
 	t.trace(func() {
-		fmt.Print(chalk.White)
+		end := time.Now()
+		statusColor := colorForStatus("info")
+
+		fmt.Printf("[%v] |%s %s %s| ",
+			end.Format("2006/01/02 - 15:04:05"),
+			statusColor, "IN", reset)
 		fmt.Println(a...)
-		fmt.Print(chalk.Reset)
 	})
 }
 
@@ -52,9 +88,13 @@ func (t Parrot) Debug(a ...interface{}) {
 	t.trace(
 		func() {
 			if t.debugMode {
-				fmt.Print(chalk.Green)
+				end := time.Now()
+				statusColor := colorForStatus("debug")
+
+				fmt.Printf("[%v] |%s %s %s| ",
+					end.Format("2006/01/02 - 15:04:05"),
+					statusColor, "DG", reset)
 				fmt.Println(a...)
-				fmt.Print(chalk.Reset)
 			}
 		})
 }
